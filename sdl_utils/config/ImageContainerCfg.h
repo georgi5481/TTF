d45dbdd0abd0ad3a8f7c3dee7d654377e3b47e06
George <georgi5481@abv.bg>
2022-05-22 12:17:19 +0300

    added a configuration for easy implement of imageContainer

---
+++ sdl_utils/config/ImageContainerCfg.h
@@ -0,0 +1,29 @@
+#ifndef SDL_UTILS_CONFIG_IMAGECONTAINERCFG_H_
+#define SDL_UTILS_CONFIG_IMAGECONTAINERCFG_H_
+
+//C system includes
+
+//C++ system includes
+#include<cstdint>
+#include<unordered_map>
+#include<string>
+//3rd-party includes
+
+//Own includes
+#include"utils/drawings/Rectangle.h"
+
+//Forward Declaration
+
+struct ImageCfg {
+	std::string location;
+	int32_t width = 0;
+	int32_t height = 0;
+};
+
+struct ImageContainerCfg{
+	std::unordered_map<int32_t, ImageConfig> imageConfigs;
+};
+
+
+
+#endif /* SDL_UTILS_CONFIG_IMAGECONTAINERCFG_H_ */
--- sdl_utils/containers/ImageContainer.h
+++ sdl_utils/containers/ImageContainer.h
@@ -10,12 +10,19 @@
 
 //Own includes
 #include "utils/drawings/Rectangle.h"
+#include "utils/config/ImageContainerCfg.h"
+
 //Forward Declaration
 
 struct SDL_Texture;
 
 class ImageContainer {
 public:
+
+	int32_t init(const ImageContainerCfg& cfg);
+	int32_t deinit();
+
+
 	SDL_Texture* getImageTexture(int32_t rsrcId) const;	//will basically search in the map below ( _textures )
 
 	Rectangle getImageFrame(int32_t rsrcId) const;		//this one will search in the other map below ( _textureFrames )
