ceeb4e8b030ad52ea960bee7162b2c41249fe0d0
George <georgi5481@abv.bg>
2022-05-29 23:12:47 +0300

    added a map implementation

--- Engine/EngineConfigLoader.cpp
+++ Engine/EngineConfigLoader.cpp
@@ -9,11 +9,21 @@
 
 
 //Own includes
+#include "common/commonDefines.h"
 
+//constants
 namespace{		//the anonymous namespace will work only on this .cpp file
 constexpr auto WINDOW_WIDTH = 640;
 constexpr auto WINDOW_HEIGHT = 480;
 constexpr auto WINDOW_NAME = "Hardware_Redering";
+
+
+constexpr auto RSRC_WIDTH = 640;
+constexpr auto RSRC_HEIGHT = 480;
+
+constexpr auto LAYER_2_IMG_WIDTH = 640;
+constexpr auto LAER_2_IMG_HEIGHT = 480;
+
 }
 
 static void populateMonitorConfig(MonitorWindowCfg& cfg){
@@ -32,6 +42,13 @@ static void populateGameConfig(GameCfg& cfg){
 */
 
 static void populateImageContainerConfig(ImageContainerCfg& cfg){
+
+	ImageCfg imageCfg;
+	imageCfg.location = "../resources/up.png";
+	imageCfg.height = RSRC_HEIGHT;
+	imageCfg.width = RSRC_WIDTH;
+
+	cfg.imageConfigs.insert(std::make_pair(TextureId::UP,imageCfg));	//we have to pair them first with the ID
 
 	cfg.imgPaths[UP] = "../resources/up.png";
 	cfg.imgPaths[DOWN] = "../resources/down.png";
---
+++ common/commonDefines.h
@@ -0,0 +1,30 @@
+#ifndef COMMON_COMMONDEFINES_H_
+#define COMMON_COMMONDEFINES_H_
+
+//C system includes
+
+//C++ system includes
+#include<cstdint>
+
+//3rd-party includes
+
+//Own includes
+
+namespace TextureId {
+
+enum ResourceId {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	PRESS_KEYS,
+	LAYER_2
+};
+
+
+}	//end of namespace TextureId
+
+
+
+
+#endif /* COMMON_COMMONDEFINES_H_ */
