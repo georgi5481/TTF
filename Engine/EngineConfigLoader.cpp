6082d356e239172763ced057ecea283102b4ab4c
George <georgi5481@abv.bg>
2022-07-14 19:14:08 +0300

    casual save

--- Engine/EngineConfigLoader.cpp
+++ Engine/EngineConfigLoader.cpp
@@ -10,7 +10,7 @@
 
 //Own includes
 #include "common/commonDefines.h"
-
+#include "sdl_utils/containers/TextContainer.h"
 //constants
 namespace{		//the anonymous namespace will work only on this .cpp file
 constexpr auto WINDOW_WIDTH = 800;
@@ -24,8 +24,17 @@ constexpr auto PRESS_KEYS_HEIGHT = 480;
 constexpr auto LAYER_2_IMG_WIDTH = 160;
 constexpr auto LAER_2_IMG_HEIGHT = 120;
 
+
+constexpr auto ANGELINE_VINTAGE_40_FONTSIZE = 40;	//we can load the same font with a different size аt the same time
 }
 
+static std::string getFilePath(const std::string& relativePath){
+#ifdef RELEASE_BUILD
+	return relativePath;
+#else
+	return"../" + relativePath;
+#endif
+}
 static void populateMonitorConfig(MonitorWindowCfg& cfg){
 	cfg.windowName = WINDOW_NAME;
 	cfg.windowWidth = WINDOW_WIDTH;
@@ -45,9 +54,25 @@ static void populateGameConfig(GameCfg& cfg){
 static void populateImageContainerConfig(ImageContainerCfg& cfg){
 
 	ImageCfg imageCfg;
-	imageCfg.location = "../resources/pictures/press_keys.png";
+	imageCfg.location = getFilePath("resources/p/press_keys.png");
 	imageCfg.height = PRESS_KEYS_WIDTH;
 	imageCfg.width = PRESS_KEYS_HEIGHT;
+
+	cfg.imageConfigs.insert(std::make_pair(TextureId::PRESS_KEYS,imageCfg));	//we have to pair them first with the ID
+
+
+	imageCfg.location = getFilePath("resources/pictures/layer_2.png");
+	imageCfg.height = LAYER_2_IMG_WIDTH;
+	imageCfg.width = LAER_2_IMG_HEIGHT;
+
+	cfg.imageConfigs.insert(std::make_pair(TextureId::LAYER_2,imageCfg));	//we have to pair them first with the ID
+}
+
+
+static void populateTextContainerConfig(TextContainerCfg& cfg){
+	FontCfg fontCfg;
+	fontCfg.location = getFilePath("/resources/fonts/AngelineVintage.ttf");
+	fontCfg.fontSize = ANGELINE_VINTAGE_40_FONTSIZE;
 
 
 	cfg.imageConfigs.insert(std::make_pair(TextureId::PRESS_KEYS,imageCfg));	//we have to pair them first with the ID
