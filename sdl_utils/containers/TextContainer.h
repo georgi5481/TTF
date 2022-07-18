ef9acaced8b3866cf7ed1c5f7621a01bf82be334
George <georgi5481@abv.bg>
2022-07-18 19:08:18 +0300

    forgot two functions

--- sdl_utils/containers/TextContainer.h
+++ sdl_utils/containers/TextContainer.h
@@ -14,7 +14,7 @@
 
 
 //Forward Declaration
-struct SDL_Textures;
+struct SDL_Texture;
 struct Color;
 typedef struct _TTF_Font TTF_Font;
 
@@ -31,9 +31,11 @@ public:
 	void reloadText(const char* text, const Color & color, int32_t fontId,
 					int32_t &outTextId, int32_t &outTextWidth, int32_t &outTextHeight);
 
+	void unloadText(int32_t textId);
 
+	SDL_Texture* getTextTexture(int32_t textId) const;
 private:
-	std::vector<SDL_Textures*> _textures; //the textures we will be drawning
+	std::vector<SDL_Texture*> _textures; //the textures we will be drawning
 
 	std::unordered_map<int32_t, TTF_Font*> _fonts;
 };
